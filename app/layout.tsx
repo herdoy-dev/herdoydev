684c09b3c18397a35fd5c4f27bf2d5a36be35151
= <=>
2025-07-09 16:21:31 +0600

    Add Chatbot

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono, Poppins } from "next/font/google";
 import { Footer } from "./footer";
 import "./globals.css";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -104,6 +105,38 @@ export default function RootLayout({
         <Navbar />
         {children}
         <Footer />
+
+        {/* Chatbase Script */}
+        <Script id="chatbase-script" strategy="afterInteractive">
+          {`
+            (function(){
+              if(!window.chatbase||window.chatbase("getState")!=="initialized"){
+                window.chatbase=(...arguments)=>{
+                  if(!window.chatbase.q){window.chatbase.q=[]}
+                  window.chatbase.q.push(arguments)
+                };
+                window.chatbase=new Proxy(window.chatbase,{
+                  get(target,prop){
+                    if(prop==="q"){return target.q}
+                    return(...args)=>target(prop,...args)
+                  }
+                })
+              }
+              const onLoad=function(){
+                const script=document.createElement("script");
+                script.src="https://www.chatbase.co/embed.min.js";
+                script.id="EzjgCYNBOzKGiouzgN3xx";
+                script.domain="www.chatbase.co";
+                document.body.appendChild(script)
+              };
+              if(document.readyState==="complete"){
+                onLoad()
+              }else{
+                window.addEventListener("load",onLoad)
+              }
+            })();
+          `}
+        </Script>
       </body>
     </html>
   );
