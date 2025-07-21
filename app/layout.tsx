23a0c09cfca03f7fb75de1d68879444a6a46f16f
= <=>
2025-07-21 22:37:40 +0600

    add google meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -103,6 +103,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="scroll-smooth">
       <head>
+        <meta name="google-adsense-account" content="ca-pub-7106488480723857" />
         <Script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7106488480723857"
