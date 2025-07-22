04b0e3da3228bace51456af67ea72de7b97b8c98
= <=>
2025-07-22 22:15:52 +0600

    Add Google Ad Script

--- app/layout.tsx
+++ app/layout.tsx
@@ -105,10 +105,8 @@ export default function RootLayout({
       <head>
         <meta name="google-adsense-account" content="ca-pub-7106488480723857" />
         <Script
-          async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7106488480723857"
           crossOrigin="anonymous"
-          strategy="afterInteractive"
         />
       </head>
       <body
